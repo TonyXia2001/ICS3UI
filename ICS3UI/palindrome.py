c667bc2099fb5a135e8a2ca356a164cbcf5482af
Tony <summertony717@gmail.com>
2018-04-11 16:24:15 -0400

    condensed palindrome

--- ICS3UI/palindrome.py
+++ ICS3UI/palindrome.py
@@ -4,20 +4,10 @@ Tony(Tanglin) Xia
 Palindrome
 take an input, detect whether it's a palindrome, return True or False
 '''
-def main():
-    userInput = input("Please type in your word:")
-    isPalindrome(userInput)
-
 def isPalindrome(userInput):
     tempList = list(userInput)
-    for i in range(len(tempList)):
-        tempList[i] = tempList[i].upper()
-    tempList2 = list(tempList)
-    tempList2.reverse()
-    if tempList2 == tempList:
-        print(userInput + " is a palindrome.")
-    else:
-        print(userInput + " is not a palindrome.")
-
-if __name__ == '__main__':
-    main()
+    for i in range(len(tempList)): tempList[i] = tempList[i].upper()
+    if tempList == tempList[::-1]: print(userInput + " is a palindrome.")
+    else: print(userInput + " is not a palindrome.")
+userInput = input("Please type in your word:")
+isPalindrome(userInput)
